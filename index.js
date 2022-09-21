badbbb82a4ee014b49caa4ad386e6a1b70ffe7ea
aegislsy <leesaeyim@naver.com>
2022-09-21 10:35:26 +0900

    search chat js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const menuItems = document.querySelectorAll(".menu-item");
 //MESSAGES
 const messagesNotification = document.querySelector("#messages-notification");
 const messages = document.querySelector(".messages");
+const message = messages.querySelectorAll(".message");
+const messageSearch = document.querySelector("#message-search");
 
 // remove active class from all menu items
 const changeActiveItem = () => {
@@ -28,6 +30,23 @@ menuItems.forEach((item) => {
 });
 
 /* =================== MESSAGES ===================== */
+
+// search chats
+const searchMessage = () => {
+  const val = messageSearch.value.toLowerCase();
+  message.forEach((chat) => {
+    let name = chat.querySelectorAll("h5").textContent.toLowerCase();
+    if (name.indexOf(val) != -1) {
+      chat.style.display = "flex";
+    } else {
+      chat.style.display = "none";
+    }
+  });
+};
+// search chat
+messageSearch.addEventListener("keyup", searchMessage);
+
+// highlight messages card when messages menu item is clicked
 messagesNotification.addEventListener("click", () => {
   messages.style.boxShadow = "0 0 1rem var(--color-primary";
   messagesNotification.querySelector(".notification-count").style.display =
@@ -36,3 +55,5 @@ messagesNotification.addEventListener("click", () => {
     messages.style.boxShadow = "none";
   }, 2000);
 });
+
+//THEME/DISPLAY CUSTOMIZATION
